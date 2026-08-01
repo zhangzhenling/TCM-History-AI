248486db218b1ec932c7fe16ef00343cdaf01443
zhangzhenling <zhangzhenling@users.noreply.github.com>
2026-08-01 03:53:30 +0000

    fix: rename commitlint.config.js to .cjs for ESM compatibility

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,24 +1,7 @@
+/** @type {import('@commitlint/types').Configuration} */
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'build',
-        'ci',
-        'chore',
-        'revert',
-      ],
-    ],
-    'subject-case': [0],
-    'body-max-line-length': [0],
+    'header-max-length': [2, 'always', 200],
   },
 };
--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('@commitlint/types').Configuration} */
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'header-max-length': [2, 'always', 200],
-  },
-};
